f557c3df54baed8f04d6ba956260efdecf47edb1
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-03 19:56:11 -0300

    Add Atividade 9 - Conversão de Moeda 2.cpp

---
+++ Atividade 9 - Conversão de Moeda 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+  
+  float dolar,rl;
+
+  printf("Digite o valor em Dolares para conversao: ");
+  scanf ("%f",&dolar);
+
+  rl = dolar * 2.40; //Conversão de dólar para real
+
+  printf("O Valor em Reais e: %3.2f ", rl);
+
+  return 0;
+}
+
