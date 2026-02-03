b0dc1570492b973916e54a2d6d1594f687d6766d
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-03 19:51:01 -0300

    Update and rename Atividade_7_31032025_Algoritmo.cpp to Atividade 7 - Coelhos.cpp

--- Atividade 7 - Coelhos.cpp
+++ Atividade 7 - Coelhos.cpp
@@ -7,10 +7,11 @@ int main (){
 	printf("insira o numero de coelhos(Co): ");
 	scanf("%f", &Co);
 
-	C = (Co * 0.70) / 18 + 10;
+	C = (Co * 0.70) / 18 + 10; //Formula para o cálculo do custo de criação de coelhos
 	
 	printf ("O numero de coelhos e:\n %.0f", Co);
 	printf ("\nO valor do custo(C) de criacao de coelhos e:\n %f", C);
 	
 	return 0;
 }
+
