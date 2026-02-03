ab0215896e9dba96fb6d080502a7f755a39b760e
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-03 19:50:02 -0300

    Add Atividade 6 - Cálculo de Prestação.cpp

--- Atividade 6 - Cálculo de Prestação.cpp
+++ Atividade 6 - Cálculo de Prestação.cpp
@@ -11,9 +11,10 @@ int main (){
 	printf("insira o valor de TEM: ");
 	scanf("%f", &TEM);
 	
-	P = V + (V * (T / 100) * TEM);
+	P = V + (V * (T / 100) * TEM); //Fórmula da Prestação
 	
 	printf ("O valor da Prestacao(P) e:\n %f", P);
 	
 	return 0;
 }
+
