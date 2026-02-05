889b1b4de525d50589722395eb4f1a4e65d53972
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-05 19:43:04 -0300

    Update and rename 6 Atividade - Cubificação e Localização de valores - 06052025.cpp to Atividade 70 - Cubificação e Localização de valores.cpp

--- Atividade 70 - Cubificação e Localização de valores.cpp
+++ Atividade 70 - Cubificação e Localização de valores.cpp
@@ -9,9 +9,9 @@ int main (){
 	for(i = 0; i <= 2; ++i){
 		printf("\n Digite um numero: ");
 		scanf("%d", &A[i]);
-		B[i] = pow(A[i], 3); /*Cubificação e armazenamento de A[i] em B[i]*/
+		B[i] = pow(A[i], 3); /*CubificaÃ§Ã£o e armazenamento de A[i] em B[i]*/
 	}
-	/*Bubble Sort de B[i] em comparação ao B[j] passando as informações de um para o outro com o auxiliar x*/
+	/*Bubble Sort de B[i] em comparaÃ§Ã£o ao B[j] passando as informaÃ§Ãµes de um para o outro com o auxiliar x*/
 	for(i = 0; i <= 1; ++i)
 	for(j = j + 1; j <= 2; ++j){
 		if(B[i] > B[j]){
@@ -21,14 +21,14 @@ int main (){
 		}
 	}
 	for(j = 0; j <= 2; ++j){
-		printf("\n %d", B[j]); /*Exibição de B[j]*/
+		printf("\n %d", B[j]); /*ExibiÃ§Ã£o de B[j]*/
 	}
-	/*Pesquisa dos números condescendentes de m[j] ao escrever o número relacionado a multiplicação de 5*/
+	/*Pesquisa dos nÃºmeros condescendentes de m[j] ao escrever o nÃºmero relacionado a multiplicaÃ§Ã£o de 5*/
 	printf("\nentre com o numero a ser pesquisado: ");
 	scanf("%d", &b);
 	j = 0;
 	a = 0;
-	/*Projeção de onde o número foi encontrado (Ex: localizado em: 1)*/
+	/*ProjeÃ§Ã£o de onde o nÃºmero foi encontrado (Ex: localizado em: 1)*/
 	while (j <= 2 && a == 0){
 		if (B[j] == b){
 			a = 1;
@@ -37,7 +37,7 @@ int main (){
 			j = j + 1;
 		}
 		if (a == 1)
-		printf ("\nFoi localizado em: %d", j + 1); /*Identificação espacial de onde o valor foi encontrado pela primeira vez*/
+		printf ("\nFoi localizado em: %d", j + 1); /*IdentificaÃ§Ã£o espacial de onde o valor foi encontrado pela primeira vez*/
 	}
 	return 0;
 }
