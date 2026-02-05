7743ac024a32c198598b9a0a5dfcf8b69e2c7e51
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-05 16:50:00 -0300

    Update and rename Atividade_7_08042025_Algoritmo.cpp to Atividade 51 - Elevação ao cubo.cpp

--- Atividade 51 - Elevação ao cubo.cpp
+++ Atividade 51 - Elevação ao cubo.cpp
@@ -6,9 +6,10 @@ int main(){
 	int cont;
 	float r;
 	
-	for (cont = 0; cont <= 15; cont ++);
+	for (cont = 0; cont <= 15; cont ++); //Realizará a função abaixo 16 vezes
 	
 	r = pow (3, cont);
 	printf("%.2f \n", r);
 	
 }
+
