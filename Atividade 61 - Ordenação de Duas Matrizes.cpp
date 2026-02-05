c176d15fdaf3db378130a13b2fa7fcc740b10306
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-05 19:31:39 -0300

    Update and rename Atividade_4_15042025_Algoritmo.cpp to Atividade 61 - Ordenação de Duas Matrizes.cpp

--- Atividade 61 - Ordenação de Duas Matrizes.cpp
+++ Atividade 61 - Ordenação de Duas Matrizes.cpp
@@ -5,14 +5,14 @@ int main (){
 	int a[5], b[5], c[10], i;
 	
 	printf("Digite os dados da primeira matriz:\n");
-	for (i = 0; i <= 4; ++i){
+	for (i = 0; i <= 4; ++i){ //Valores inseridos da Matriz A
 		printf("Numero %i: ", i + 1);
 		scanf("%i", &a[i]);
 		c[i] = a[i];
 	}
 	
 	printf("Digite os dados da segunda matriz:\n");
-	for (i = 0; i <= 4; ++i){
+	for (i = 0; i <= 4; ++i){ //Valores inseridos da Matriz B
 		printf("Numero %i: ", i + 1);
 		scanf("%i", &b[i]);
 		c[i + 5] = b[i];
@@ -20,7 +20,8 @@ int main (){
     
     printf("A matriz C e:\n");
     for (i = 0; i <= 9; ++i){
-    	printf("Numero %i: %i\n", i + 1, c[i]);
+    	printf("Numero %i: %i\n", i + 1, c[i]); //Junção e Ordenação das matrizes em ordem crescente
 	}
 	return 0;
 }
+
