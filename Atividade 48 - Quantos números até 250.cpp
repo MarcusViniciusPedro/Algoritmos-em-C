6c78deb8e3dc28e6bc09f61e8b0a5b8ff18449e3
Marcus Vinícius <marcusviniciuspedro11@gmail.com>
2026-02-05 16:38:09 -0300

    Update and rename Atividade_4_08042025_Algoritmo.cpp to Atividade 48 - Quantos números até 250.cpp

--- Atividade 48 - Quantos números até 250.cpp
+++ Atividade 48 - Quantos números até 250.cpp
@@ -9,10 +9,11 @@ int main(){
 	
 	do{
 		printf("\n %i", n);
-		n = n * 3;
+		n = n * 3; //multiplica o valor dado pelo usuário por 3 até que o valor alcance ou seja um pouco menor que 250
 	}
 	
 	while (n < 250);
 	
 	return 0;
 }
+
